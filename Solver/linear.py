90bdc5f105826c68c840296b25656e81d0b7dbcc
ferdean <deandresvertferran@gmail.com>
2025-06-19 14:28:32 +0000

    Improved linear solver

--- Solver/linear.py
+++ Solver/linear.py
@@ -8,7 +8,7 @@ import scipy.sparse.linalg
 
 import dolfinx.fem as dfem
 
-from FEM.operators import StokesAssembler
+from FEM.operators import BaseAssembler
 from FEM.plot import plot_mixed_function
 
 logger = logging.getLogger(__name__)
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 class LinearSolver:
     """Container for different linear-solving algorithms."""
 
-    def __init__(self, assembler: StokesAssembler):
+    def __init__(self, assembler: BaseAssembler) -> None:
         """Initialize."""
         self.assembler = assembler
         self._ab_cache: dict[str, tuple] = {}
@@ -26,17 +26,19 @@ class LinearSolver:
     def direct_lu_solve(
         self, *, show_plot: bool = True, plot_scale: float = 0.1
     ) -> dfem.Function:
-        """Direct solve using direct sparse LU (splu)."""
-        if (key := "direct_lu") not in self._ab_cache:
+        """Direct solve using sparse LU (splu)."""
+        logger.info("Direct LU solve started.")
+        key = "direct_lu"
+        if key not in self._ab_cache:
             logger.debug("Assembling system matrix and RHS for direct LU")
-            A, b = self.assembler.assemble()
+            A, b = self.assembler.get_matrix_forms()
             self._ab_cache[key] = (A, b)
         else:
             logger.debug("Using cached A and b for direct LU")
             A, b = self._ab_cache[key]
 
         if key not in self._lu_cache:
-            logger.debug("Factoring system matrix.")
+            logger.debug("Factoring (LU) system matrix.")
             self._lu_cache[key] = scipy.sparse.linalg.splu(A.as_scipy_array().tocsc())
         else:
             logger.debug("Using cached LU factorization")
@@ -48,12 +50,15 @@ class LinearSolver:
             raise RuntimeError("LU solve failed") from e
 
         wh = self.assembler.sol
-
         wh.x.array[:] = x
         wh.x.scatter_forward()
 
         if show_plot:
             plot_mixed_function(wh, plot_scale)
 
-        logger.info("Direct LU solve completed")
+        logger.info("Direct LU solve completed.")
         return wh
+
+
+# TODO: Extend with iterative solvers like GMRES, CG, etc.
+# TODO: Integrate PETSc built-in solvers for better performance
