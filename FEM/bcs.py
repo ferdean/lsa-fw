bf83b84745897508f44a2c3195c7f4de2d1a90e1
ferdean <deandresvertferran@gmail.com>
2025-07-18 06:44:48 +0000

    Fixed typehint

--- FEM/bcs.py
+++ FEM/bcs.py
@@ -55,7 +55,7 @@ class BoundaryCondition:
     type: BoundaryConditionType
     """Type of boundary condition."""
     value: (
-        float | tuple[int, int] | tuple[float, ...] | Callable[[np.ndarray], np.ndarray]
+        float | tuple[int, ...] | tuple[float, ...] | Callable[[np.ndarray], np.ndarray]
     )
     """Value of the boundary condition.
 
